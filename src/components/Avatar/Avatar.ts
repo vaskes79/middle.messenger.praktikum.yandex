1b3732604525f830bc4c2dc542db10c53fbe5edd
Vasily Guzov <vasily.guzov@gmail.com>
2023-01-11 23:17:20 +0300

    [component] improve avatar

--- src/components/Avatar/Avatar.ts
+++ src/components/Avatar/Avatar.ts
@@ -1,13 +1,16 @@
-import tmpl from 'bundle-text:./Avatar.html'
+import tmpl from 'bundle-text:./Avatar.html';
+import defaultAvatar from './default-avatar.svg';
 
 type Attributes = 'imgurl' | 'name'
 
 export class Avatar extends HTMLElement {
-  _imgUrl: string;
-  _name: string;
+  _imgUrl: string = defaultAvatar;
+  _name: string = "Display Name";
+  _initial: string = "DN"
 
   _container: HTMLDivElement;
   _img: HTMLImageElement;
+  _nameEl: HTMLElement;
 
   constructor() {
     super();
@@ -15,17 +18,34 @@ export class Avatar extends HTMLElement {
     if (this.shadowRoot) {
       this.shadowRoot.innerHTML = tmpl;
     }
-
-    // set elements
-    this._img = this.shadowRoot?.querySelector(".img") as HTMLImageElement;
-    this._container = this.shadowRoot?.querySelector('.container') as HTMLDivElement;
-    // set Attributes 
-    this._imgUrl = this.getAttribute('imgurl') || "";
-    this._name = this.getAttribute('name') || "";
-
-    this._img.setAttribute("alt", this._name);
-    this._img.setAttribute("src", this._imgUrl);
   }
+
+  connectedCallback() {
+    if (this.shadowRoot) {
+      // set elements
+      this._img = this.shadowRoot?.querySelector(".img") as HTMLImageElement;
+      this._container = this.shadowRoot?.querySelector('.container') as HTMLDivElement;
+      this._nameEl = this.shadowRoot?.querySelector('.name') as HTMLElement;
+      // set Attributes 
+      this._imgUrl = this.getAttribute('imgurl') || this._imgUrl;
+      this._name = this.getAttribute('name') || this._name;
+
+      this._img.setAttribute("alt", this._name);
+      this._img.setAttribute("src", this._imgUrl);
+
+      if (!this.hasAttribute('imgurl')) {
+        this._createInitials();
+        this._nameEl.innerHTML = this._initial;
+        this._nameEl.style.display = "flex";
+      }
+    }
+  }
+
+  _createInitials = () => {
+    const [firstName, lastName] = this._name.split(' ')
+    this._initial = firstName[0] + lastName[0];
+  }
+
 }
 
 export default customElements.define('ypr-avatar', Avatar);
