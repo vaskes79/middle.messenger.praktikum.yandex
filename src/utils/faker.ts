938956cc2532ec305bdb57619490fad2beae4f95
Vasily Guzov <vasily.guzov@gmail.com>
2023-01-13 19:55:52 +0300

    [dependencies] replaced faker, make build huge

--- src/utils/faker.ts
+++ src/utils/faker.ts
@@ -1,51 +1,53 @@
-import { faker } from '@faker-js/faker';
-import { format } from 'date-fns'
+import { rand, randImg, randNumber, randBetweenDate, randSentence, randParagraph, randFullName, randAvatar } from '@ngneat/falso';
+
+import format from 'date-fns/format';
 import { OwnerMessage, TypeContentMessage } from '../components/MessageItem/MessageItem';
 import { StatusUserValue, StatusMessageState } from '../components/Status';
 
 export function randomStatusUser() {
-  return faker.helpers.arrayElement<StatusUserValue>(["online", "empty", "ofline", "not-set"]);
+  return rand<StatusUserValue>(["online", "empty", "ofline", "not-set"]);
 }
 
 export function randomStatusMessage() {
-  return faker.helpers.arrayElement<StatusMessageState>(["read", "seen", "sent"]);
+  return rand<StatusMessageState>(["read", "seen", "sent"]);
 }
 
 export function randomOwnerMessage() {
-  return faker.helpers.arrayElement<OwnerMessage>(["me", "user"]);
+  return rand<OwnerMessage>(["me", "user"]);
 }
 export function randomTypeMessage() {
-  return faker.helpers.arrayElement<TypeContentMessage>(["text", "image"]);
+  return rand<TypeContentMessage>(["text", "image"]);
 }
 
 export function randomImage() {
-  return faker.image.abstract(undefined, undefined, true);
+  return randImg();
 }
 
 export function randomNumber() {
-  return faker.helpers.arrayElement([faker.random.numeric(), faker.random.numeric(2)])
+  return randNumber({ min: 0, max: 99 });
 }
 
 export function randomTime(formatTime?: string) {
   formatTime = formatTime || "hh:mm";
-  return format(faker.datatype.datetime(), formatTime);
+  const date = randBetweenDate({ from: new Date('10/07/2021'), to: new Date() })
+  return format(date, formatTime);
 }
 
 export function randomFullName() {
-  return faker.name.fullName();
+  return randFullName();
 }
 
 export function randomAvatar() {
-  return faker.image.avatar();
+  return randAvatar()
 }
 
 export function randomWords(wordCount?: number) {
-  wordCount = wordCount || 5;
-  return faker.random.words(wordCount);
+  const length = wordCount || 5;
+  return randSentence({ length })
 }
 
 export function randomMessage() {
-  return faker.lorem.sentences();
+  return randParagraph();
 }
 
 export function generateItems<Type = any>(itemGenerator: () => Type, itemNumber?: number) {
