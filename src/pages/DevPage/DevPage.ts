df2c9f8bba87107eb4b2fda38e72d5955750d6dd
Vasily Guzov <vasily.guzov@gmail.com>
2023-01-14 20:15:01 +0300

    [refactoring] DevPage

--- src/pages/DevPage/DevPage.ts
+++ src/pages/DevPage/DevPage.ts
@@ -3,17 +3,6 @@ function buildSettingsPage() {
   return html;
 }
 
-export function demoModals() {
-  const btnOpenModal = document.getElementById("openModalBtn");
-  const modal: any = document.getElementById('modal');
-  btnOpenModal?.addEventListener('click', () => {
-    console.log('btnOpenModal')
-    if (modal) {
-      modal.open();
-    }
-  })
-}
-
 export function DevPage() {
   return buildSettingsPage();
 }
