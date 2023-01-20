db1519cd124f135e2597e745cdf5a7596887e7de
Vasily Guzov <vasily.guzov@gmail.com>
2023-01-20 13:11:35 +0300

    [EventBus] #61 draft implementation

---
+++ src/core/EventBus.ts
@@ -0,0 +1,52 @@
+import { ChatPageEvent } from '../pages/ChatPage/ChatPage';
+
+export type EventName = keyof GlobalEventHandlersEventMap | `${ChatPageEvent}`;
+export type Callback<T = any> = (...args: T[]) => void;
+type ListenersList = Record<EventName, Callback[]>;
+
+export class EventBus {
+  private static _instance: EventBus = new EventBus();
+  private _listeners = {} as ListenersList;
+
+  constructor() {
+    if (EventBus._instance) {
+      this._error('Instantiation failed: Use EventBus.getInstance() instead of new.');
+    }
+    EventBus._instance = this;
+  }
+
+  private _error(msg?: string) {
+    msg = `Error: ${msg}` || 'Error: EventBus';
+    throw new Error(msg);
+  }
+
+  public static getInstance(): EventBus {
+    return EventBus._instance;
+  }
+
+  on<T>(event: EventName, callback: Callback<T>) {
+    if (!this._listeners[event]) {
+      this._listeners[event] = [];
+    }
+
+    this._listeners[event].push(callback);
+  }
+
+  off<T>(event: EventName, callback: Callback<T>) {
+    if (!this._listeners[event]) {
+      this._error(`Event ${event} not found`);
+    }
+
+    this._listeners[event] = this._listeners[event].filter((listener) => listener !== callback);
+  }
+
+  emmit<T>(event: EventName, ...args: T[]) {
+    if (!this._listeners[event]) {
+      this._error(`Event ${event} not found`);
+    }
+
+    this._listeners[event].forEach((listener) => {
+      listener(...args);
+    });
+  }
+}
