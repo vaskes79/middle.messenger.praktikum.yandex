d059eea0b3ae563a7fd80b4111e3858b975a4148
Vasily Guzov <vasily.guzov@gmail.com>
2023-01-29 00:37:01 +0300

    [#55][HTTPTransport] chec POST request

--- src/pages/DevPage/handlers.ts
+++ src/pages/DevPage/handlers.ts
@@ -3,18 +3,35 @@ import { HTTPTransport } from '../../core/HTTPTransport';
 import { Handlers } from '../../types';
 
 const eventBuss = EventBus.getInstance();
+type TodoItem = {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+};
+
+type DTOTodoItem = Omit<TodoItem, 'id'>;
 
 const handleModal = () => {
   eventBuss.emmit('modal:open', 'id');
 };
 
-async function testGetRequest() {
-  const data = await HTTPTransport.GET<{
-    id: number;
-    title: string;
-    body: string;
-    userId: number;
-  }>('https://jsonplaceholder.typicode.com/posts/1');
+// async function testGetRequest() {
+//   const data = await HTTPTransport.GET<TodoItem>('https://jsonplaceholder.typicode.com/posts/1');
+//   console.log({ data });
+// }
+
+async function testPostRequest() {
+  const data = await HTTPTransport.POST<DTOTodoItem, { id: number }>(
+    'https://jsonplaceholder.typicode.com/posts',
+    {
+      data: {
+        title: 'foo',
+        body: 'bar',
+        userId: 1
+      }
+    }
+  );
   console.log({ data });
 }
 
@@ -29,7 +46,8 @@ export const handlers: Handlers[] = [
     selector: '#btn',
     handler: () => {
       console.log('testGetRequest');
-      testGetRequest();
+      // testGetRequest();
+      testPostRequest();
     }
   }
 ];
