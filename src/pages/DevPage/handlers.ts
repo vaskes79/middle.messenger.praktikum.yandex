d6be48a9f3e5ba3b51e54d4197beea6e058cb1e8
Vasily Guzov <vasily.guzov@gmail.com>
2023-01-29 02:04:30 +0300

    [#55] check PUT request

--- src/pages/DevPage/handlers.ts
+++ src/pages/DevPage/handlers.ts
@@ -21,12 +21,26 @@ const handleModal = () => {
 //   console.log({ data });
 // }
 
-async function testPostRequest() {
-  const data = await HTTPTransport.POST<DTOTodoItem, { id: number }>(
-    'https://jsonplaceholder.typicode.com/posts',
+// async function testPostRequest() {
+//   const data = await HTTPTransport.POST<DTOTodoItem, { id: number }>(
+//     'https://jsonplaceholder.typicode.com/posts',
+//     {
+//       data: {
+//         title: 'foo',
+//         body: 'bar',
+//         userId: 1
+//       }
+//     }
+//   );
+//   console.log({ data });
+// }
+
+async function testPutRequest() {
+  const data = await HTTPTransport.PUT<DTOTodoItem, { id: number }>(
+    'https://jsonplaceholder.typicode.com/posts/1',
     {
       data: {
-        title: 'foo',
+        title: 'foo000',
         body: 'bar',
         userId: 1
       }
@@ -47,7 +61,8 @@ export const handlers: Handlers[] = [
     handler: () => {
       console.log('testGetRequest');
       // testGetRequest();
-      testPostRequest();
+      // testPostRequest();
+      testPutRequest();
     }
   }
 ];
