7d033bf5ee404a2afc8414da6ba1ff8f583a0769
Vasily Guzov <vasily.guzov@gmail.com>
2023-01-29 20:23:26 +0300

    [#50][preview] setup submit handlers for the MessageInput

--- src/components/MessageInput/MessageInput.ts
+++ src/components/MessageInput/MessageInput.ts
@@ -1,12 +1,15 @@
 import html from 'bundle-text:./MessageInput.html';
 import css from 'bundle-text:./MessageInput.css';
 import { BaseComponent } from '../../core';
+import { handlers } from './handlers';
 
 const tagName = 'ypr-message-input';
 
 export class MessegaInput extends BaseComponent {
+  value = '';
+
   constructor() {
-    super({ html, css, tagName });
+    super({ html, css, tagName, handlers });
   }
 }
 
