00960a818e32af77e275563755fff464154896f3
Vasily Guzov <vasily.guzov@gmail.com>
2023-01-29 20:04:05 +0300

    [#50] handling submit form logic

--- src/components/Form/Form.ts
+++ src/components/Form/Form.ts
@@ -1,39 +1,34 @@
 import html from 'bundle-text:./Form.html';
 import css from 'bundle-text:./Form.css';
 import { BaseComponent, Validator } from '../../core';
+import { nanoid } from 'nanoid';
 import { Input } from '../Input';
 
 const tagName = 'ypr-form';
 
 export class Form extends BaseComponent {
-  _form: HTMLFormElement;
-  _submitBtn: HTMLButtonElement;
-  _inputs: Input[];
+  private _inputs: Input[];
+  id: string = nanoid();
 
   constructor() {
     super({ html, css, tagName });
-    this._form = this._root.querySelector('form') as HTMLFormElement;
-    this._submitHandler = this._submitHandler.bind(this);
-    const footerSlot = this._root.querySelector('slot[name="footer"]') as HTMLSlotElement;
-    this._submitBtn = footerSlot
-      .assignedElements()[0]
-      .querySelector('[type="submit"]') as HTMLButtonElement;
     const contentSlot = this._root.querySelector('slot[name="content"]') as HTMLSlotElement;
     this._inputs = contentSlot.assignedElements() as Input[];
   }
 
   protected _mount(): void {
-    this._submitBtn.addEventListener('click', this._submitHandler);
+    this.setAttribute('id', this.id);
+    this._eventBuss.on('submit', (id: string) => {
+      if (id === this.id) {
+        this._submitHandler();
+      }
+    });
   }
 
-  protected _unmount(): void {
-    this._submitBtn.addEventListener('click', this._submitHandler);
-  }
-
-  _submitHandler(event: Event) {
-    event.preventDefault();
+  private _submitHandler() {
+    const data: { name: string; value: string }[] = [];
     this._inputs.forEach((input) => {
-      const { value = '', _typeOfValidate } = input;
+      const { value = '', _typeOfValidate, name } = input;
       const isNotEmpty = Validator.isNotEmpty(value);
       const valid = Validator[_typeOfValidate](value);
       if (!valid) {
@@ -42,7 +37,14 @@ export class Form extends BaseComponent {
       if (!isNotEmpty) {
         input.showError('Inupt should field');
       }
+      if (valid || isNotEmpty) {
+        data.push({ name, value });
+      }
     });
+    const formIsValid = this._inputs.length === data.length;
+    if (formIsValid) {
+      console.log(data);
+    }
   }
 }
 
