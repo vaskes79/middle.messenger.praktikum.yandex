b1c0229fa6a4efb1aaede7b705525b0f1efc1f69
Vasily Guzov <vasily.guzov@gmail.com>
2023-01-29 17:04:39 +0300

    [#50] correct regex for login

--- src/core/Validator/regExps.ts
+++ src/core/Validator/regExps.ts
@@ -1,6 +1,6 @@
 export const emailRegExp = /^\w+([.-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/;
 export const displayNameRegExp = /^[A-ZА-Я][a-zа-яё]+$/;
-export const loginRegExp = /(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9_-]{3,20}/;
-export const passRegExp = /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,40}/;
+export const loginRegExp = /^[a-zA-Z0-9_-]{3,20}$/;
+export const passRegExp = /(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,40}/;
 export const phoneRegExp = /^\+?[0-9]{10,15}$/;
 export const notEmptyRegExp = /\w+\S+/g;
