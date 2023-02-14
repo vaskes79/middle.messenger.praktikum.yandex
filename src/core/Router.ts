f02bf7a9286b3a57363f5f040a19f7837ce0f383
Vasily Guzov <vasily.guzov@gmail.com>
2023-02-14 20:47:21 +0300

    [Router] fix forward behavior

--- src/core/Router.ts
+++ src/core/Router.ts
@@ -17,17 +17,14 @@ export class Router {
 
   static use(path: Paths, attrs?: Record<string, string>) {
     const route = new Route(path, attrs);
-
     this._instance._routes[path] = route;
     return this;
   }
 
   private _locationPathHandler = (event: PopStateEvent) => {
-    console.log('_locationPathHandler', event);
     const { currentTarget } = event;
-    console.log('w.h.state :', window.history.state);
     const pathname = (currentTarget as Window).location.pathname;
-    this._navigate(pathname);
+    Router.go(pathname);
   };
 
   private _navigate(path: string) {
@@ -46,22 +43,19 @@ export class Router {
   static start() {
     window.addEventListener('popstate', this._instance._locationPathHandler);
     const path = window.location.pathname;
-    this.go(path);
     this._instance._navigate(path);
-    console.log('START', this._instance._history);
-    return this;
   }
 
   static go(pathname: string) {
-    this._instance._history.pushState({}, '', pathname);
     this._instance._navigate(pathname);
+    this._instance._history.pushState({}, pathname, pathname);
   }
 
   static back() {
-    this._instance._history.go(-1);
+    this._instance._history.back();
   }
 
   static forward() {
-    this._instance._history.go(1);
+    this._instance._history.forward();
   }
 }
