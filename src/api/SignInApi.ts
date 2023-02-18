f376bc54bb8011a6d620e8d3e6747fdae4316a75
Vasily Guzov <vasily.guzov@gmail.com>
2023-02-18 18:26:29 +0300

    [api][SignInApi] implementation

---
+++ src/api/SignInApi.ts
@@ -0,0 +1,13 @@
+import { BaseAPI } from '../core';
+import type { UserLoginDTO } from '../types';
+
+export class SignInApi extends BaseAPI {
+  constructor(baseUrl?: string) {
+    super('/auth/signin', baseUrl);
+  }
+
+  async create(userData: UserLoginDTO): Promise<void> {
+    const data = await this._http.POST<UserLoginDTO, void>(this._url, { data: userData });
+    console.log('SignInApi: ', data);
+  }
+}
