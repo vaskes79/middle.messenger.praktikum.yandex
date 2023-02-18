d02a515ba3cabcd3f49d123564282a0db28f19bc
Vasily Guzov <vasily.guzov@gmail.com>
2023-02-18 15:11:58 +0300

    [preview][SignUpApi] class

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import './pages';
 import { Paths } from './types';
 import { Router } from './core';
 import { setupRootEventListeners } from './initHandlers';
+import { API } from './api';
 
 setupRootEventListeners();
 
@@ -12,3 +13,12 @@ Router.use(Paths.signIn)
   .use(Paths.chat, { private: 'true' })
   .use(Paths.dev)
   .start();
+
+API.signUp.create({
+  first_name: 'Vasily',
+  second_name: 'Perov',
+  login: 'vasily-perov',
+  email: 'vasily-perov@mail.com',
+  password: '1979dfcz',
+  phone: '+79111111111'
+});
