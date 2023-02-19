856dab7f2df290ba3ae66a108e32602c4be0b842
Vasily Guzov <vasily.guzov@gmail.com>
2023-02-19 14:43:57 +0300

    [api][AddUserToChatApi] add users to chat

--- src/api/chats/index.ts
+++ src/api/chats/index.ts
@@ -1,7 +1,9 @@
 import { GetAllChatsApi } from './GetAllChatsApi';
 import { CreateChatDTO, CreateChatApi } from './CreateChatApi';
+import { AddUserToChatApi, AddUserToChatDTO } from './AddUserToChatApi';
 
 export const chats = {
   getAllChats: () => new GetAllChatsApi().request(),
-  createChat: (chat: CreateChatDTO) => new CreateChatApi().create(chat)
+  createChat: (chat: CreateChatDTO) => new CreateChatApi().create(chat),
+  addUserToChat: (data: AddUserToChatDTO) => new AddUserToChatApi().update(data)
 };
