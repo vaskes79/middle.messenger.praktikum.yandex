edff7f3cdfb5c021cd0212f99570d563d44d6e21
Vasily Guzov <vasily.guzov@gmail.com>
2023-02-20 21:24:04 +0300

    [api][UserAvatarUpdateApi]

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,5 +1,7 @@
+import { UserAvatarUpdateDTO, UserAvatarUpdateApi } from './UserAvatarUpdate';
 import { UserProfileApi, UpdateUserDTO } from './UserProfileApi';
 
 export const user = {
-  updateProfile: (userData: UpdateUserDTO) => new UserProfileApi().update(userData)
+  updateProfile: (userData: UpdateUserDTO) => new UserProfileApi().update(userData),
+  updateAvatar: (avatarData: UserAvatarUpdateDTO) => new UserAvatarUpdateApi().update(avatarData)
 };
