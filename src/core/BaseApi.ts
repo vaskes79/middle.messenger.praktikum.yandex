f80774bc47e4fca1fd0b02b0b4a200845feca436
Vasily Guzov <vasily.guzov@gmail.com>
2023-02-26 13:44:32 +0300

    [BaseApi] imporvment add store to class property

--- src/core/BaseApi.ts
+++ src/core/BaseApi.ts
@@ -1,7 +1,9 @@
 import { EventBus } from './EventBus';
 import { HTTPTransport, OptionsWithoutMethod } from './HTTPTransport';
+import { Store } from './Store';
 
 const BASE_URL = 'https://ya-praktikum.tech/api/v2';
+export const BASE_WS_URL = 'wss://ya-praktikum.tech/ws/chats';
 
 export interface BaseAPIInterface {
   request?(opt?: OptionsWithoutMethod<unknown>): Promise<void>;
@@ -14,11 +16,13 @@ export abstract class BaseAPI {
   protected _url: string;
   protected _eventBus: EventBus;
   protected _http: typeof HTTPTransport;
+  protected _store: typeof Store;
 
   constructor(url: string, baseURL?: string) {
     baseURL = baseURL || BASE_URL;
     this._url = `${baseURL}${url}`;
     this._eventBus = EventBus.getInstance();
     this._http = HTTPTransport;
+    this._store = Store;
   }
 }
