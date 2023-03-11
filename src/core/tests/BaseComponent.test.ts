c41b5a7b09f0aab7aef07408c5e7aa5be5e45b71
Vasily Guzov <vasily.guzov@gmail.com>
2023-03-11 13:54:00 +0300

    [test] instaceof BaseComponent, create HTMLElement

--- src/core/tests/BaseComponent.test.ts
+++ src/core/tests/BaseComponent.test.ts
@@ -1,8 +1,37 @@
 import { expect } from 'chai';
+import { BaseComponent } from '../BaseComponent';
 
 describe('BaseComponent Tests', () => {
-  it('draft test', () => {
-    const result = 1 + 3;
-    expect(result).to.equal(4);
+  const html = `<div><button id="btn">test button</button></div>`;
+  const css = `div {background-color: red}`;
+  const tagName = `ypr-test-class`;
+
+  class TestClass extends BaseComponent {
+    private _btnEl: HTMLButtonElement;
+
+    constructor() {
+      super({ html, css, tagName });
+      this._btnEl = this._root.querySelector('#btn') as HTMLButtonElement;
+    }
+
+    protected _mount(): void {
+      this._btnEl.addEventListener('click', () => 'OK');
+    }
+  }
+
+  before(() => {
+    window.customElements.define(tagName, TestClass);
+    const buttonElem = document.createElement('ypr-test-class');
+    document.body.appendChild(buttonElem);
+  });
+
+  describe('Initialisation Tests', () => {
+    it('shoul be instance of BaseComponent', () => {
+      expect(TestClass).instanceof(BaseComponent.constructor);
+    });
+
+    it('should create html element ypr-test-class', () => {
+      expect(document.body.innerHTML).to.contain('<ypr-test-class></ypr-test-class>');
+    });
   });
 });
